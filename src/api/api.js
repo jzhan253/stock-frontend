a3e763434518424ad9899c29dce88b4681f1813c
Jinfan <kimthebestman@gmail.com>
2021-12-12 02:18:35 -0800

    111

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,20 @@
 
-export const allocate = async (userInput) => {
+export const suggest = async (userInput) => {
 
-    return fetch('/allocate', {
+    return fetch('/suggest', {
         method: 'POST',
         body: JSON.stringify(userInput),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+export const suggest = async () => {
+
+    return fetch('/weeklytrend', {
+        method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
